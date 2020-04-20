21e8312b0787bbce75b30d134607eee3003b5dd6
Reece <rkeeyuna@gmail.com>
2020-04-20 13:20:52 -0700

    updated

--- app.js
+++ app.js
@@ -2,30 +2,30 @@ document.addEventListener("keydown", changeSize);
 
 function changeSize() {
   var e = event.keyCode;
-  var bSize = document.getElementById("balloonImg").style.fontSize;
+  var bSize = document.getElementById("balloon").style.fontSize;
   if (e == 73) {
     if (parseInt(bSize) > 60) {
-      document.getElementById("balloonImg").innerHTML = "💥";
+      document.getElementById("balloon").innerHTML = "💥";
       document.removeEventListener("keyup", changeSize);
     } else {
       if (bSize == "") {
         bSize = 30;
       }
       var inflateSize = parseInt(bSize) + 5;
-      document.getElementById("balloonImg").style.fontSize =
+      document.getElementById("balloon").style.fontSize =
         inflateSize.toString() + "px";
     }
   } else if (e == 68) {
-    console.log("d was pressed");
+    console.log("d insert");
     if (parseInt(bSize) == 0) {
-      document.getElementById("balloonImg").innerHTML = "Done";
+      document.getElementById("balloon").innerHTML = "Done";
       document.removeEventListener("keyup", changeSize);
     } else {
       if (bSize == "") {
         bSize = 30;
       }
       var deflateSize = parseInt(bSize) - 10;
-      document.getElementById("balloonImg").style.fontSize =
+      document.getElementById("balloon").style.fontSize =
         deflateSize.toString() + "px";
     }
   }
